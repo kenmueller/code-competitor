f2703f5a09fecef4a57f695be88d0bf7bd1bec29
Ken Mueller <kenmueller0@gmail.com>
2020-09-13 12:07:38 -0700

    info title

---
+++ public/components/Info.tsx
@@ -0,0 +1,11 @@
+import styles from 'styles/components/Info.module.scss'
+
+const Info = () => (
+	<section className={styles.root}>
+		<h3 className={styles.title}>
+			We will <strong>arm</strong> you with the <em>computer science<br /> and programming knowledge, skills, and tools</em><br /> to <strong>compete</strong> at the highest high school level
+		</h3>
+	</section>
+)
+
+export default Info
