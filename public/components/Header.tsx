802c19f47c19bbc288ce88f33247b9d79a5ce54d
Ken Mueller <kenmueller0@gmail.com>
2020-09-13 11:36:55 -0700

    header content

--- public/components/Header.tsx
+++ public/components/Header.tsx
@@ -1,8 +1,29 @@
+import Link from 'next/link'
+import cx from 'classnames'
+
 import styles from 'styles/components/Header.module.scss'
 
 const Header = () => (
 	<header className={styles.root}>
-		
+		<h1 className={styles.title}>
+			From 0 to Competitive
+		</h1>
+		<p className={styles.subtitle}>
+			Creating apps won't get you into a <em>Top University</em><br />
+			Placing in a <strong>Coding Competition</strong> can
+		</p>
+		<nav className={styles.navigation}>
+			<Link href="/">
+				<a className={cx(styles.navigationLink, styles.startNavigationLink)}>
+					Start with Basics
+				</a>
+			</Link>
+			<Link href="/">
+				<a className={cx(styles.navigationLink, styles.catalogNavigationLink)}>
+					Course Catalog
+				</a>
+			</Link>
+		</nav>
 	</header>
 )
 
