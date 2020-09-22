f260ae81b9522ce459d71f4caa88c6053b1f32ef
Ken Mueller <kenmueller0@gmail.com>
2020-09-21 22:59:51 -0700

    info responsive

--- public/components/Home/Info.tsx
+++ public/components/Home/Info.tsx
@@ -1,3 +1,9 @@
+import { Svg } from 'react-optimized-image'
+import cx from 'classnames'
+
+import trophy from 'images/icons/trophy.svg'
+import strength from 'images/icons/strength.svg'
+
 import styles from 'styles/components/Info.module.scss'
 
 const Info = () => (
@@ -7,11 +13,13 @@ const Info = () => (
 		</h3>
 		<div className={styles.boxes}>
 			<article className={styles.box}>
+				<Svg className={cx(styles.boxIcon, styles.boxIcon_left)} src={trophy} />
 				<p className={styles.boxTitle}>
 					If you'd like to <span className={styles.boxTitleEmphasized}>compete</span><br />you will be <em>well-prepared</em>
 				</p>
 			</article>
 			<article className={styles.box}>
+				<Svg className={cx(styles.boxIcon, styles.boxIcon_right)} src={strength} />
 				<p className={styles.boxTitle}>
 					If you want the <span className={styles.boxTitleEmphasized}>skills</span>, without the<br />competition, you will gain <em>problem solving<br />and coding</em> abilities for <em>any</em> project
 				</p>
