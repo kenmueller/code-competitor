f6d07f315b446b583340b8cfccfdeafbedec8e92
Ken Mueller <kenmueller0@gmail.com>
2020-09-23 12:55:39 -0700

    article classname

--- public/components/Article/index.tsx
+++ public/components/Article/index.tsx
@@ -1,11 +1,16 @@
 import { PropsWithChildren } from 'react'
+import cx from 'classnames'
 
 import CustomMDXProvider from './CustomMDXProvider'
 
 import styles from 'styles/components/Article.module.scss'
 
-const Article = ({ children }: PropsWithChildren<{}>) => (
-	<article className={styles.root}>
+export interface ArticleProps extends PropsWithChildren<{}> {
+	className?: string
+}
+
+const Article = ({ className, children }: ArticleProps) => (
+	<article className={cx(styles.root, className)}>
 		<CustomMDXProvider>{children}</CustomMDXProvider>
 	</article>
 )
