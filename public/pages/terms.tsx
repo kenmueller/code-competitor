8f46806a9287ac0b303d2d279fc7f518283689fa
Ken Mueller <kenmueller0@gmail.com>
2020-09-23 13:57:33 -0700

    terms page

---
+++ public/pages/terms.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+
+import Navbar from 'components/Navbar'
+import Header from 'components/Header'
+import Breadcrumbs from 'components/Breadcrumbs'
+import Main from 'components/Terms/Main'
+import Subscribe from 'components/Subscribe'
+import Footer from 'components/Footer'
+
+const FAQ = () => (
+	<>
+		<Head>
+			<title key="title">
+				Terms of Service - Code Competitor
+			</title>
+		</Head>
+		<Navbar light />
+		<Header title="Terms of Service" />
+		<Breadcrumbs
+			trail={[{ url: '/', title: 'Home' }]}
+			title="Terms"
+		/>
+		<Main />
+		<Subscribe />
+		<Footer />
+	</>
+)
+
+export default FAQ
