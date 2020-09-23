e4758425a60b70ebed57398a08b87a2c4f1c5ab6
Ken Mueller <kenmueller0@gmail.com>
2020-09-22 21:25:58 -0700

    instructor model

---
+++ public/models/Instructor.ts
@@ -0,0 +1,10 @@
+export default interface Instructor {
+	name: string
+	bio: string
+	cppExperience: string
+	academicBackground: string
+	competitionBackground: string
+	openSourceProjects: string
+	pastEmployers: string
+	hobbies: string
+}
