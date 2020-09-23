6bcb46a9f17b2e315805a784b7148403b957c288
Ken Mueller <kenmueller0@gmail.com>
2020-09-23 14:39:52 -0700

    pre no class name

--- public/components/Article/Pre.tsx
+++ public/components/Article/Pre.tsx
@@ -1,28 +1,33 @@
 import Highlight, { defaultProps, Language } from 'prism-react-renderer'
 
+import styles from 'styles/components/Article/Pre.module.scss'
+
 export interface PreProps {
-	className: string
+	className?: string
 	children: string
 }
 
-const Pre = ({ className, children }: PreProps) => {
-	const language = className.replace(/^language\-/, '') as Language
-	
-	return (
-		<Highlight {...defaultProps} code={children} language={language}>
-			{({ className, tokens, getLineProps, getTokenProps }) => (
-				<pre className={className}>
-					{tokens.map((line, i) => (
-						<div key={i} {...getLineProps({ line, key: i })}>
-							{line.map((token, key) => (
-								<span key={key} {...getTokenProps({ token, key })} />
-							))}
-						</div>
-					))}
-				</pre>
-			)}
-		</Highlight>
-	)
-}
+const Pre = ({ className, children }: PreProps) =>
+	className
+		? (
+			<Highlight
+				{...defaultProps}
+				code={children}
+				language={className?.replace(/^language\-/, '') as Language}
+			>
+				{({ className, tokens, getLineProps, getTokenProps }) => (
+					<pre className={className}>
+						{tokens.map((line, i) => (
+							<div key={i} {...getLineProps({ line, key: i })}>
+								{line.map((token, key) => (
+									<span key={key} {...getTokenProps({ token, key })} />
+								))}
+							</div>
+						))}
+					</pre>
+				)}
+			</Highlight>
+		)
+		: <pre className={styles.plain}>{children}</pre>
 
 export default Pre
