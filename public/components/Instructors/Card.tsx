75b665f8028dc33c2e0baea142da0408c07bc3e8
Ken Mueller <kenmueller0@gmail.com>
2020-09-23 11:50:43 -0700

    instructors card

---
+++ public/components/Instructors/Card.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import Img from 'react-optimized-image'
+
+import { BasicInstructor } from 'models/Instructor'
+
+import styles from 'styles/components/Instructors/Card.module.scss'
+
+export interface InstructorsMainCardProps {
+	user: BasicInstructor
+}
+
+const InstructorsMainCard = ({ user }: InstructorsMainCardProps) => (
+	<Link href="/instructors/[slug]" as={`/instructors/${user.slug}`}>
+		<a className={styles.root}>
+			<div className={styles.imageContainer}>
+				<Img
+					className={styles.image}
+					src={require(`images/instructors/${user.slug}.jpg`)}
+					alt={user.name}
+					webp
+				/>
+			</div>
+			<p className={styles.name}>{user.name}</p>
+		</a>
+	</Link>
+)
+
+export default InstructorsMainCard
