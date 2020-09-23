0a2834a925a7d3b5837ec0cad460c807fb6c4c3d
Ken Mueller <kenmueller0@gmail.com>
2020-09-23 13:40:10 -0700

    classes

---
+++ public/components/Home/Classes.tsx
@@ -0,0 +1,30 @@
+import Img from 'react-optimized-image'
+
+import image from 'images/home/classes.png'
+
+import styles from 'styles/components/Home/Classes.module.scss'
+
+const HomeClasses = () => (
+	<section className={styles.root}>
+		<Img
+			className={styles.image}
+			src={image}
+			alt="Classes"
+			webp
+		/>
+		<article className={styles.content}>
+			<h3 className={styles.title}>Classes</h3>
+			<ul className={styles.list}>
+				<li><p className={styles.item}>3 to 8 students</p></li>
+				<li><p className={styles.item}>90 minute sessions</p></li>
+				<li><p className={styles.item}>2 to 4 hours homework</p></li>
+				<li><p className={styles.item}>Office hours</p></li>
+				<li><p className={styles.item}>1-on-1 tutoring</p></li>
+				<li><p className={styles.item}>Discussion forum</p></li>
+				<li><p className={styles.item}>Grades</p></li>
+			</ul>
+		</article>
+	</section>
+)
+
+export default HomeClasses
