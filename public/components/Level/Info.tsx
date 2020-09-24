74bc65eeea958c7528325f3f38daeaf8a50cf8b1
Ken Mueller <kenmueller0@gmail.com>
2020-09-24 12:34:59 -0700

    gift

--- public/components/Level/Info.tsx
+++ public/components/Level/Info.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router'
+import { Svg } from 'react-optimized-image'
 
 import Article from 'components/Article'
+import gift from 'images/icons/gift.svg'
 
 import styles from 'styles/components/Level/Info.module.scss'
 
@@ -41,7 +43,20 @@ const LevelInfo = () => {
 				</div>
 			</div>
 			<aside className={styles.aside}>
-				
+				<article className={styles.info}>
+					
+				</article>
+				<article className={styles.gift}>
+					<Svg className={styles.giftIcon} src={gift} />
+					<p className={styles.giftMessage}>
+						If you can’t afford to pay for this course, please email <a
+							className={styles.giftMessageLink}
+							href="mailto:learn@codecompetitor.com"
+						>
+							learn@codecompetitor.com
+						</a>. Nobody should miss out on these incredibly valuable skills just due to money.
+					</p>
+				</article>
 			</aside>
 		</section>
 	)
