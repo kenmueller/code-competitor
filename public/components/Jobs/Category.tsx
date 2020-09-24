7484af344b8fd50694876d4fc3cd3041cca65635
Ken Mueller <kenmueller0@gmail.com>
2020-09-23 23:59:24 -0700

    category

---
+++ public/components/Jobs/Category.tsx
@@ -0,0 +1,19 @@
+import { JobCategory } from 'models/Job'
+import Job from './Job'
+
+import styles from 'styles/components/Jobs/Category.module.scss'
+
+export interface JobsCategoryProps {
+	category: JobCategory
+}
+
+const JobsCategory = ({ category }: JobsCategoryProps) => (
+	<section className={styles.root}>
+		<h2 className={styles.title}>{category.name}</h2>
+		{category.jobs.map(job => (
+			<Job key={job.slug} category={category} job={job} />
+		))}
+	</section>
+)
+
+export default JobsCategory
