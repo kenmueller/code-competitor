8d69823deb38bf6ac78c79edba81bb9ded592e1c
Ken Mueller <kenmueller0@gmail.com>
2020-09-25 13:27:04 -0700

    info rows and link

--- public/components/Level/Info.tsx
+++ public/components/Level/Info.tsx
@@ -1,6 +1,8 @@
+import { FC } from 'react'
 import { useRouter } from 'next/router'
 import { Svg } from 'react-optimized-image'
 
+import Level from 'models/Level'
 import Article from 'components/Article'
 import gift from 'images/icons/gift.svg'
 
@@ -8,8 +10,14 @@ import styles from 'styles/components/Level/Info.module.scss'
 
 const LevelInfo = () => {
 	const slug = useRouter().query.slug as string
+	const {
+		default: Description,
+		meta: level
+	} = require(`articles/levels/${slug}/index.mdx`) as {
+		default: FC
+		meta: Level
+	}
 	
-	const Description = require(`articles/levels/${slug}/index.mdx`).default
 	const Who = require(`articles/levels/${slug}/who.mdx`).default
 	const What = require(`articles/levels/${slug}/what.mdx`).default
 	const Why = require(`articles/levels/${slug}/why.mdx`).default
@@ -44,7 +52,27 @@ const LevelInfo = () => {
 			</div>
 			<aside className={styles.aside}>
 				<article className={styles.info}>
-					
+					<h3 className={styles.infoTitle}>Course Info</h3>
+					<div className={styles.rows}>
+						<p className={styles.row}>
+							<span className={styles.key}>Usaco level</span>
+							<span className={styles.value}>{level.usacoLevel}</span>
+						</p>
+						<hr className={styles.divider} />
+						<p className={styles.row}>
+							<span className={styles.key}>Ages</span>
+							<span className={styles.value}>{level.ages.min}-{level.ages.max}</span>
+						</p>
+						<hr className={styles.divider} />
+						<p className={styles.row}>
+							<span className={styles.key}>Book required</span>
+							<span className={styles.value}>{level.bookRequired ? 'Yes' : 'No'}</span>
+						</p>
+					</div>
+					<p className={styles.price}>${level.price}</p>
+					<a className={styles.scheduleLink} href="#">
+						Class schedule
+					</a>
 				</article>
 				<article className={styles.gift}>
 					<Svg className={styles.giftIcon} src={gift} />
