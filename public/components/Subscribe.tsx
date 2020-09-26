471b4d788dd1ab3a423fdf30b57a426d594b77c8
Ken Mueller <kenmueller0@gmail.com>
2020-09-26 16:26:27 -0700

    mailchimp

--- public/components/Subscribe.tsx
+++ public/components/Subscribe.tsx
@@ -2,6 +2,9 @@ import { ChangeEvent, useCallback, useState } from 'react'
 
 import styles from 'styles/components/Subscribe.module.scss'
 
+const FORM_ACTION = 'https://codecompetitor.us2.list-manage.com/subscribe/post?u=3fd10344aec009bbd8873553b&id=74e41d2051'
+const FAKE_INPUT_NAME = 'b_3fd10344aec009bbd8873553b_74e41d2051'
+
 const Subscribe = () => {
 	const [email, setEmail] = useState('')
 	
@@ -14,14 +17,21 @@ const Subscribe = () => {
 			<h3 className={styles.title}>
 				Subscribe for news & updates
 			</h3>
-			<form className={styles.form}>
+			<form className={styles.form} action={FORM_ACTION} method="POST" target="_blank">
 				<input
 					className={styles.input}
 					required
 					type="email"
+					name="EMAIL"
 					placeholder="Email"
 					value={email}
 					onChange={onInputChange}
+				/>
+				<input
+					name={FAKE_INPUT_NAME}
+					tabIndex={-1}
+					hidden
+					aria-hidden
 				/>
 				<button className={styles.button}>
 					Submit
