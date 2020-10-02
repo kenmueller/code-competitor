0ec514edd9ef3f5d3b8e263255d48af6e557d9d8
Ken Mueller <kenmueller0@gmail.com>
2020-10-01 23:48:07 -0700

    menu

--- public/components/Navbar.tsx
+++ public/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState, useCallback, useEffect } from 'react'
 import Link from 'next/link'
 import Img, { Svg } from 'react-optimized-image'
 import cx from 'classnames'
@@ -10,25 +11,100 @@ export interface NavbarProps {
 	light?: boolean
 }
 
-const Navbar = ({ light = false }: NavbarProps) => (
-	<nav className={styles.root}>
-		<Link href="/">
-			<a>
-				<Img
-					className={styles.logo}
-					src={require(`images/logos/${light ? 'white' : 'blue'}.png`)}
-					alt="Logo"
-					webp
-				/>
+interface NavbarMenuLinkProps {
+	title: string
+	href: string
+	onClick(): void
+}
+
+const NavbarMenuLink = ({ title, href, onClick }: NavbarMenuLinkProps) => (
+	<div className={styles.menuLinkContainer}>
+		<Link href={href}>
+			<a className={styles.menuLink} onClick={onClick}>
+				{title}
 			</a>
 		</Link>
-		<button className={styles.menu}>
-			<Svg
-				className={cx(styles.menuIcon, { [styles.lightMenuIcon]: light })}
-				src={menu}
-			/>
-		</button>
-	</nav>
+	</div>
 )
+
+const Navbar = ({ light = false }: NavbarProps) => {
+	const menuTrigger = useRef<HTMLButtonElement | null>(null)
+	const menuContent = useRef<HTMLElement | null>(null)
+	
+	const [isMenuShowing, setIsMenuShowing] = useState(false)
+	
+	const toggleIsMenuShowing = useCallback(() => {
+		setIsMenuShowing(isShowing => !isShowing)
+	}, [setIsMenuShowing])
+	
+	const onBodyClick = useCallback(({ target }: MouseEvent) => {
+		const trigger = menuTrigger.current
+		const content = menuContent.current
+		
+		if (!(
+			trigger === content ||
+			trigger?.contains(target as Node) ||
+			content === target ||
+			content?.contains(target as Node)
+		))
+			setIsMenuShowing(false)
+	}, [menuContent, setIsMenuShowing])
+	
+	useEffect(() => {
+		if (!isMenuShowing)
+			return
+		
+		const { body } = document
+		
+		body.addEventListener('click', onBodyClick)
+		
+		return () => body.removeEventListener('click', onBodyClick)
+	}, [isMenuShowing, onBodyClick])
+	
+	return (
+		<nav className={styles.root}>
+			<Link href="/">
+				<a>
+					<Img
+						className={styles.logo}
+						src={require(`images/logos/${light ? 'white' : 'blue'}.png`)}
+						alt="Logo"
+						webp
+					/>
+				</a>
+			</Link>
+			<div className={styles.menu}>
+				<button
+					ref={menuTrigger}
+					className={styles.menuTrigger}
+					onClick={toggleIsMenuShowing}
+					aria-haspopup
+				>
+					<Svg
+						className={cx(styles.menuTriggerIcon, {
+							[styles.lightMenuTriggerIcon]: light
+						})}
+						src={menu}
+					/>
+				</button>
+				<nav
+					ref={menuContent}
+					className={cx(styles.menuContent, {
+						[styles.menuContentShowing]: isMenuShowing
+					})}
+					aria-hidden={!isMenuShowing}
+				>
+					<NavbarMenuLink title="Courses" href="/courses" onClick={toggleIsMenuShowing} />
+					<NavbarMenuLink title="Competitions" href="/competitions" onClick={toggleIsMenuShowing} />
+					<NavbarMenuLink title="Instructors" href="/instructors" onClick={toggleIsMenuShowing} />
+					<NavbarMenuLink title="About" href="/about" onClick={toggleIsMenuShowing} />
+					<NavbarMenuLink title="FAQ" href="/faq" onClick={toggleIsMenuShowing} />
+					<NavbarMenuLink title="Jobs" href="/jobs" onClick={toggleIsMenuShowing} />
+					<NavbarMenuLink title="Terms" href="/terms" onClick={toggleIsMenuShowing} />
+				</nav>
+			</div>
+		</nav>
+	)
+}
 
 export default Navbar
