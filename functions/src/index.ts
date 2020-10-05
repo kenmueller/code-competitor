680b9d51087d2a9eb4e7d8c8fcd5f6d94a030323
Ken Mueller <kenmueller0@gmail.com>
2020-10-04 23:01:27 -0700

    export create stripe session

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,3 +3,5 @@ import { initializeApp } from 'firebase-admin'
 initializeApp({
 	storageBucket: 'code-competitor.appspot.com'
 })
+
+export { default as createStripeSession } from './createStripeSession'
