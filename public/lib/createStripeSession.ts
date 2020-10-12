4f0c38f4040decfbb3461e08f5c8728934d4c7fc
Ken Mueller <kenmueller0@gmail.com>
2020-10-12 15:44:36 -0700

    create stripe session lazy load and calls function

--- public/lib/createStripeSession.ts
+++ public/lib/createStripeSession.ts
@@ -1,6 +1,8 @@
-// import { loadStripe } from '@stripe/stripe-js'
+import { Stripe } from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js/pure'
 
 import firebase from './firebase'
+import handleError from './handleError'
 
 import 'firebase/functions'
 
@@ -8,24 +10,24 @@ interface CreateStripeSession {
 	(type: 'level', data: { level: number }): Promise<void>
 }
 
-// const _stripe = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY)
 const _createStripeSession = firebase.functions().httpsCallable('createStripeSession')
+
+let _stripe: Promise<Stripe> | null = null
 
 const createStripeSession: CreateStripeSession = async (type, data) => {
 	try {
-		// const stripe = await _stripe
+		const stripe = await (_stripe ??= loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY))
 		const sessionId = await _createStripeSession({ type, ...data })
-		console.log(sessionId)
-		// if (typeof sessionId !== 'string')
-		// 	throw new Error('An unknown error occurred')
 		
-		// const { error } = await stripe.redirectToCheckout({ sessionId })
+		if (typeof sessionId !== 'string')
+			throw new Error('An unknown error occurred')
 		
-		// if (error)
-		// 	throw error
+		const { error } = await stripe.redirectToCheckout({ sessionId })
+		
+		if (error)
+			throw error
 	} catch (error) {
-		console.error(error)
-		alert(error.message)
+		handleError(error)
 	}
 }
 
