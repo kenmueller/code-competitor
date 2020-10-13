458c5ddb30ceadd7e2ea33c2236f05510e0dd930
Ken Mueller <kenmueller0@gmail.com>
2020-10-12 21:38:39 -0700

    get static props passes number

--- public/lib/level/getStaticProps.tsx
+++ public/lib/level/getStaticProps.tsx
@@ -3,12 +3,19 @@ import { GetStaticProps } from 'next'
 import { readdirSync } from 'fs'
 import { join } from 'path'
 import stripHtml from 'string-strip-html'
+import levelSlugToNumber from 'lib/levelSlugToNumber'
 
 const getStaticProps: GetStaticProps = async ({ params: { slug } }) => {
+	const number = levelSlugToNumber(slug as string)
+	
+	if (number === null)
+		throw new Error(`Invalid level slug "${slug}"`)
+	
 	const Description = require(`articles/levels/${slug}/index.mdx`).default
 	
 	return {
 		props: {
+			number,
 			description: stripHtml(renderToStaticMarkup(<Description />)).result,
 			lastSession: readdirSync(join(process.cwd(), `articles/levels/${slug}/sessions`))
 				.reduce((max, path) => (
