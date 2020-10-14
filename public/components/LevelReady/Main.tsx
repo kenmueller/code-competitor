65ef58fd9b8aaf20363defda2e5f097e361a12f2
Ken Mueller <kenmueller0@gmail.com>
2020-10-14 12:56:11 -0700

    notice

--- public/components/LevelReady/Main.tsx
+++ public/components/LevelReady/Main.tsx
@@ -6,18 +6,20 @@ import styles from 'styles/components/LevelReady/Main.module.scss'
 
 const LevelReadyMain = () => {
 	const slug = useRouter().query.slug as string
-	const Content = require(`articles/levels/${slug}/ready.mdx`).default
+	const { notice, default: Content } = require(`articles/levels/${slug}/ready.mdx`)
 	
 	return (
 		<main className={styles.root}>
-			<p className={styles.notice}>
-				Submit C++ code to <a
-					className={styles.link}
-					href="mailto:learn@codecompetitor.com"
-				>
-					learn@codecompetitor.com
-				</a> for feedback
-			</p>
+			{(notice ?? true) && (
+				<p className={styles.notice}>
+					Submit C++ code to <a
+						className={styles.link}
+						href="mailto:learn@codecompetitor.com"
+					>
+						learn@codecompetitor.com
+					</a> for feedback
+				</p>
+			)}
 			<Article className={styles.article}>
 				<Content />
 			</Article>
