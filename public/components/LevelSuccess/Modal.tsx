3916a85a221699d5610ab14376b13fdfceba48ff
Ken Mueller <kenmueller0@gmail.com>
2020-10-27 00:26:16 -0700

    fill svg

--- public/components/LevelSuccess/Modal.tsx
+++ public/components/LevelSuccess/Modal.tsx
@@ -9,24 +9,21 @@ export interface LevelSuccessModalProps {
 	name: string
 }
 
-const LevelSuccessModal = ({ name }: LevelSuccessModalProps) => {
-	
-	return (
-		<div className={styles.root} role="presentation">
-			<div className={styles.content}>
-				<p className={styles.icon} role="img" aria-hidden>🥳</p>
-				<p className={styles.title}>
-					You've registered for <span className={styles.name}>{name}</span>
-				</p>
-				<Link href="/courses">
-					<a className={styles.link}>
-						Courses
-						<Svg className={styles.linkIcon} src={rightArrow} />
-					</a>
-				</Link>
-			</div>
+const LevelSuccessModal = ({ name }: LevelSuccessModalProps) => (
+	<div className={styles.root} role="presentation">
+		<div className={styles.content}>
+			<p className={styles.icon} role="img" aria-hidden>🥳</p>
+			<p className={styles.title}>
+				You've registered for <span className={styles.name}>{name}</span>
+			</p>
+			<Link href="/courses">
+				<a className={styles.link}>
+					Courses
+					<Svg className={styles.linkIcon} src={rightArrow} />
+				</a>
+			</Link>
 		</div>
-	)
-}
+	</div>
+)
 
 export default LevelSuccessModal
